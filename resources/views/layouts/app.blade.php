da256e2ac3a1074e700a332c787fd6b269ac9861
Rajesh S <rajesh.s@inoru.com>
2020-08-18 22:30:03 +0530

    JS name changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,8 @@
     <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
     <link rel="stylesheet" href="{{ asset('css/zoomple.css') }}">
 
-    <!-- <link rel="stylesheet" href="http://localhost:8001/css/wheel-api-new7.css"> -->
-    <link rel="stylesheet" href="http://web9.vtdns.net/css/wheel-api-new7.css"> 
+    <!-- <link rel="stylesheet" href="http://localhost:8001/css/wheel-api-new8.css"> -->
+    <link rel="stylesheet" href="http://web9.vtdns.net/css/wheel-api-new8.css"> 
 
 
 
@@ -234,14 +234,14 @@
     });
     });
 
-    // var accesstoken= "{{@$request->accesstoken?:'Ykc5allXeG9iM04w'}}"//new7
-    var accesstoken= "{{@$request->accesstoken?:'ZDJWaU5pNTJkR1J1Y3k1dVpYUT0='}}"//new7
+    // var accesstoken= "{{@$request->accesstoken?:'Ykc5allXeG9iM04w'}}"//new8
+    var accesstoken= "{{@$request->accesstoken?:'ZDJWaU5pNTJkR1J1Y3k1dVpYUT0='}}"//new8
     // Ykc5allXeG9iM04w
     // ZDJWaU5pNTJkR1J1Y3k1dVpYUT0=
     </script>
 
-    <!-- <script src="http://localhost:8001/js/wheel-api-new7.js"></script>  -->
-    <script src="http://web9.vtdns.net/js/wheel-api-new7.js"></script> 
+    <!-- <script src="http://localhost:8001/js/wheel-api-new8.js"></script>  -->
+    <script src="http://web9.vtdns.net/js/wheel-api-new8.js"></script> 
 
 
 
