3e01ef4ef5e0b27e7cc00252a8f84bb1739aa1af
Rajesh S <rajesh.s@inoru.com>
2020-08-19 20:31:49 +0530

    First Time Loader Issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,8 +28,8 @@
      <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
      <link rel="stylesheet" href="{{ asset('css/zoomple.css') }}">
 
-     <!-- <link rel="stylesheet" href="http://localhost:8001/css/wheel-api-new16.css"> -->
-     <link rel="stylesheet" href="http://web9.vtdns.net/css/wheel-api-new16.css">
+     <!-- <link rel="stylesheet" href="http://localhost:8001/css/wheel-api-new17.css"> -->
+     <link rel="stylesheet" href="http://web9.vtdns.net/css/wheel-api-new17.css">
 
 
 
@@ -227,14 +227,14 @@
              });
          });
 
-         // var accesstoken= "{{@$request->accesstoken?:'Ykc5allXeG9iM04w'}}"//new16
-         var accesstoken = "{{@$request->accesstoken?:'ZDJWaU5pNTJkR1J1Y3k1dVpYUT0='}}" //new16
+         // var accesstoken= "{{@$request->accesstoken?:'Ykc5allXeG9iM04w'}}"//new17
+         var accesstoken = "{{@$request->accesstoken?:'ZDJWaU5pNTJkR1J1Y3k1dVpYUT0='}}" //new17
          // Ykc5allXeG9iM04w
          // ZDJWaU5pNTJkR1J1Y3k1dVpYUT0=
      </script>
 
-     <!-- <script src="http://localhost:8001/js/wheel-api-new16.js"></script>  -->
-     <script src="http://web9.vtdns.net/js/wheel-api-new16.js"></script>
+     <!-- <script src="http://localhost:8001/js/wheel-api-new17.js"></script>  -->
+     <script src="http://web9.vtdns.net/js/wheel-api-new17.js"></script>
 
      <script type="text/javascript">
      </script>
